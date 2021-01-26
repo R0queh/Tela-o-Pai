84113094a827b9ff675e186faf27be83b3a1ff04
Bruno de Oliveira <brunosrdeoliveira@gmail.com>
2021-01-26 13:13:10 -0300

    componente inicio perfil

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -8,14 +8,13 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-
   constructor(
     private router:Router
   ) { }
 
   ngOnInit() {
     if(environment.token == ''){
-      alert('Sua seção expirou, faça o login novamente')
+      // alert('Sua seção expirou, faça o login novamente')
       this.router.navigate(['/entrar'])
     }
   }
